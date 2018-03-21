47bac9c95aecbc35607f1abdf62422577890ca82
Gijs van der Giessen <QA@bolboosch.nl>
2018-03-21 16:59:29 +0100

    Tried to search for bolboosch

--- nightmarejs/index.js
+++ nightmarejs/index.js
@@ -1,12 +1,16 @@
+
 const Nightmare = require('nightmare')
 const nightmare = Nightmare({ show: true })
 
 nightmare
   .goto('https://duckduckgo.com')
-  .type('#search_form_input_homepage', 'github nightmare')
+  .type('#search_form_input_homepage', 'bolboosch')
   .click('#search_button_homepage')
   .wait('#r1-0 a.result__a')
   .evaluate(() => document.querySelector('#r1-0 a.result__a').href)
+  .click('#r1-0 a.result__a')
+  .wait('#menu')
+  .click('#menu')
   .end()
   .then(console.log)
   .catch(error => {
